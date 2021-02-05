8ee0ecc6d97e3f3823bd6e5431c0241dc41b3e03
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-02-05 11:32:39 -0800

    Revert to original test setup for Calculator to check toBeInstanceOf

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -7,6 +7,9 @@ describe('Calculator', () => {
     calculator = new Calculator(30);
   });
 
+  test('should correctly create a calculator object', () => {
+    expect(calculator).toBeInstanceOf(Calculator);
+  });
 
   test('should correctly create a calculator object with inputtedAge property', () => {
     expect(calculator.inputtedAge).toEqual(30);
@@ -14,9 +17,23 @@ describe('Calculator', () => {
 
   test('should correctly convert inputtedAge to age in number of Earth days', () => {
     expect(calculator.earthDays).toEqual(10950);
-  })
+  });
 
   test('should correctly convert inputtedAge to Mercury Age', () => {
     expect(calculator.mercuryAge).toEqual(124);
-  })
+  });
+
+  test('should correctly convert inputtedAge to Venus Age', () => {
+    expect(calculator.venusAge).toEqual(124);
+  });
+
+  test('should correctly convert inputtedAge to Mars Age', () => {
+    expect(calculator.marsAge).toEqual(124);
+  });
+
+  test('should correctly convert inputtedAge to Jupiter Age', () => {
+    expect(calculator.jupiterAge).toEqual(2.5);
+  });
+
+  
 })
