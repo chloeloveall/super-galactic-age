9e67d4b8af33964953564667bee3935ea81206ce
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-02-05 09:46:43 -0800

    Add red stage fail for calculator class with inputtedAge property

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,11 +1,6 @@
 import { Calculator } from './../src/js/calculator.js';
 
 describe('Calculator', () => {
-  // let calculator;
-
-  // beforeEach(() => {
-  //   calculator = new Calculator();
-  // });
 
   test('should correctly create a calculator object with inputtedAge property', () => {
     const calculator = new Calculator(30);
