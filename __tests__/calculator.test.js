3bc96920576999523c252815237143237c528314
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-02-05 10:06:57 -0800

    Refactor tests to include beforeEach block

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,9 +1,22 @@
 import { Calculator } from './../src/js/calculator.js';
 
 describe('Calculator', () => {
+  let calculator;
+
+  beforeEach(() => {
+    calculator = new Calculator(30);
+  });
+
 
   test('should correctly create a calculator object with inputtedAge property', () => {
-    const calculator = new Calculator(30);
     expect(calculator.inputtedAge).toEqual(30);
   });
+
+  test('should correctly convert inputtedAge to age in number of Earth days', () => {
+    expect(calculator.earthDays).toEqual(10950);
+  })
+
+  test('should correctly convert inputtedAge to Mercury Age', () => {
+    expect(calculator.mercuryAge).toEqual(124);
+  })
 })
