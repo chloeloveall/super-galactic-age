d3169f3a791c2ce0a6a9e98fff65b06a07d6e3d5
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-02-06 01:45:24 -0800

    import Calculator, build UI logic to display ages/life expectancies

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-/*eslint-disable*/
+import { Calculator } from './../src/js/calculator.js';
 import $ from 'jquery';
-/*eslint-disable*/
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+$('form#age-info').submit(function(event) {
+  event.preventDefault();
+  const inputtedAge = $('input#age').val();
+  const inputtedLifeExpectancy = $('input#lifeExpectancy').val();
+  let calculator = new Calculator(inputtedAge, inputtedLifeExpectancy);
+
+  $('#mercury-age').text(calculator.mercuryAge());
+  $('#mercury-expectancy').text(calculator.mercuryLifeExpectancy());
+
+  $('#venus-age').text(calculator.venusAge());
+  $('#venus-expectancy').text(calculator.venusLifeExpectancy());
+
+  $('#mars-age').text(calculator.marsAge());
+  $('#mars-expectancy').text(calculator.marsLifeExpectancy());
+
+  $('#jupiter-age').text(calculator.jupiterAge());
+  $('#jupiter-expectancy').text(calculator.jupiterLifeExpectancy());
+
+  $('#age-info')[0].reset();
+});
